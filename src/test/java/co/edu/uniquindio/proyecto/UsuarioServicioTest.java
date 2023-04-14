1a86a1cf6c9861c1ec78458cf3401c68c8881762
proyecto <williams.castiblancoc@uqvirtual.edu.co>
2023-04-13 22:42:20 -0500

    avance interfaces ramaWIlliam

--- src/test/java/co/edu/uniquindio/proyecto/UsuarioServicioTest.java
+++ src/test/java/co/edu/uniquindio/proyecto/UsuarioServicioTest.java
@@ -2,7 +2,6 @@ package co.edu.uniquindio.proyecto;
 
 import co.edu.uniquindio.proyecto.dto.UsuarioDTO;
 import co.edu.uniquindio.proyecto.model.Usuario;
-import co.edu.uniquindio.proyecto.servicios.interfaces.UsuarioServicio;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
