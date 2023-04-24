d371c8359e528c11fab89ae8ffcaa8e64871e100
Nicolás Jaramillo Arcos <nicolas.jaramilloa@uqvirtual.edu.co>
2023-04-24 14:44:08 -0500

    Se agregaron Domicilio, Email, Moderador, Remate y Usuario a Implementación de Servicios

---
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/implementacion/RemateServicioImpl.java
@@ -0,0 +1,21 @@
+package co.edu.uniquindio.proyecto.servicios.implementacion;
+
+import co.edu.uniquindio.proyecto.dto.ProductoGetDTO;
+import co.edu.uniquindio.proyecto.servicios.interfaces.Remate;
+
+public class RemateServicioImpl implements Remate {
+    @Override
+    public ProductoGetDTO aplicarRemate(int codigoProducto, int codigoUsuario) {
+        return null;
+    }
+
+    @Override
+    public ProductoGetDTO actualizarRemate(int codigoProducto, int codigoUsuario) {
+        return null;
+    }
+
+    @Override
+    public ProductoGetDTO eliminarRemate(int codigoProducto, int codigoUsuario) {
+        return null;
+    }
+}
