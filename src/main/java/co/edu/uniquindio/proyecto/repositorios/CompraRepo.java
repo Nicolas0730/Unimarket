81524715dde15b83afe56d87c58173e1bba800d0
Sebastian <sebastian.sabogalg@uqvirtual.edu.co>
2023-04-24 22:06:51 -0500

    Actualizacion 10:06

--- src/main/java/co/edu/uniquindio/proyecto/repositorios/CompraRepo.java
+++ src/main/java/co/edu/uniquindio/proyecto/repositorios/CompraRepo.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface CompraRepo extends JpaRepository <Compra, Integer> {
@@ -14,4 +16,7 @@ public interface CompraRepo extends JpaRepository <Compra, Integer> {
 
     @Query("select c from Compra c where c.codigo = :codigo")
     RemateUsuario obtenerCompra (int codigoCompra);
+
+    @Query ("select c from Compra c where c.codigo = :codigo")
+    List<RemateUsuario> listarCompra(int codigoCompra);
 }
