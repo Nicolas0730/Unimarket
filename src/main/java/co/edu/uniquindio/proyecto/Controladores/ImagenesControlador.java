a49d7cacec83be6111e9b6bd852cebe9178ce753
proyecto <williams.castiblancoc@uqvirtual.edu.co>
2023-04-24 21:15:10 -0500

    listarRemate

---
+++ src/main/java/co/edu/uniquindio/proyecto/Controladores/ImagenesControlador.java
@@ -0,0 +1,40 @@
+package co.edu.uniquindio.proyecto.Controladores;
+
+import co.edu.uniquindio.proyecto.dto.MensajeDTO;
+import co.edu.uniquindio.proyecto.servicios.interfaces.CloudinaryServicio;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.mapping.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+@RestController
+@RequestMapping("/api/imagenes")
+@RequiredArgsConstructor
+
+public class ImagenesControlador {
+
+    private final CloudinaryServicio cloudinaryServicio;
+
+
+    @PostMapping("/subir")
+    public ResponseEntity<MensajeDTO>subirImagen(@RequestParam("file")MultipartFile file) throws  Exception {
+        File imagen =cloudinaryServicio.convertir(file);
+        Map respuesta= (Map) cloudinaryServicio.subirImagen(imagen, "proyecto");
+        return ResponseEntity.status(HttpStatus.OK).body(new MensajeDTO(HttpStatus.OK, false, respuesta
+
+    }
+
+    @DeleteMapping("/{id}")
+    public  ResponseEntity<MensajeDTO> eliminarImagen(@PathVariable String id ) throws  Exception {
+         Map respuesta = cloudinaryServicio.eliminarImagen(id);
+         return ResponseEntity.status(HttpStatus.OK, false, respuesta)
+
+    }
+
+
+
+}
--- src/main/java/co/edu/uniquindio/proyecto/Controladores/ProductoControlador.java
+++ src/main/java/co/edu/uniquindio/proyecto/Controladores/ProductoControlador.java
@@ -13,55 +13,55 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-//@RestController
-//@RequestMapping("api/productos")
-//@AllArgsConstructor
-//public class ProductoControlador {
-//    @Autowired
-//    private final ProductoServicio.productoServicio;
-//
-//    @PostMapping("/crear")
-//    int crearProducto(ProductoDTO productoDTO) throws Exception;
-//
-//    @PostMapping("/actualizar")
-//    int actualizarProducto(int codigoProducto,ProductoDTO productoDTO) throws Exception;
-//
-//  @DeleteMapping("/eliminar")
-//    int eliminarProducto(int codigoProducto) throws Exception;
-//
-//  @GetMapping("/obtener")
-//    ProductoGetDTO obtenerProducto(int codigoProducto) throws Exception;
-//
-//  @PostMapping("agregarCarrito")
-//    int agregarCarrito(int codigoProducto)throws Exception;
-//
-//  @PostMapping("/agregarFavorito")
-//    int agregarFavorito (int codigoUsuario, int codigoProducto) throws Exception;
-//
-//  @DeleteMapping("/eliminarFavorito")
-//    int eliminarFavorito(int codigoUsuario, int codigoProducto) throws Exception;
-//
-//
-//    @GetMapping("/lista")
-//    List<ProductoGetDTO> listarProductosUsuario(int codigoUsuario) throws Exception;
-//
-//    @GetMapping("/lista")
-//    List<ProductoGetDTO> listarProductosCategoria(Categoria categoria) throws Exception;//Es una enumeracion
-//
-//    @GetMapping("/lista")
-//    List<ProductoGetDTO> listarProductosEstado(Estado estado) throws Exception;
-//
-//    @GetMapping("/lista")
-//    List<ProductoGetDTO> listarProductoNombre(String nombre) throws Exception;
-//
-//    @GetMapping("/lista")
-//    List<ProductoGetDTO> listarProductosPrecio(float precioMin, float precioMax) throws Exception;
-//
-//    @GetMapping("/lista")
-//    List<ProductoGetDTO> listarProductoFavoritos(int codigoUsuario) throws Exception;
-//
-//    @GetMapping("/lista")
-//    List<ProductoGetDTO> lsitarProductoPropio(int codigoUsuario)throws Exception;//listar los productos que el usuario publica
-//
+@RestController
+@RequestMapping("api/productos")
+@AllArgsConstructor
+public class ProductoControlador {
+    @Autowired
+    private final ProductoServicio.productoServicio;
+
+    @PostMapping("/crear")
+    int crearProducto(ProductoDTO productoDTO) throws Exception;
+
+    @PostMapping("/actualizar")
+    int actualizarProducto(int codigoProducto,ProductoDTO productoDTO) throws Exception;
+
+  @DeleteMapping("/eliminar")
+    int eliminarProducto(int codigoProducto) throws Exception;
+
+  @GetMapping("/obtener")
+    ProductoGetDTO obtenerProducto(int codigoProducto) throws Exception;
+
+  @PostMapping("agregarCarrito")
+    int agregarCarrito(int codigoProducto)throws Exception;
+
+  @PostMapping("/agregarFavorito")
+    int agregarFavorito (int codigoUsuario, int codigoProducto) throws Exception;
+
+  @DeleteMapping("/eliminarFavorito")
+    int eliminarFavorito(int codigoUsuario, int codigoProducto) throws Exception;
+
+
+    @GetMapping("/lista")
+    List<ProductoGetDTO> listarProductosUsuario(int codigoUsuario) throws Exception;
+
+    @GetMapping("/lista")
+    List<ProductoGetDTO> listarProductosCategoria(Categoria categoria) throws Exception;//Es una enumeracion
+
+    @GetMapping("/lista")
+    List<ProductoGetDTO> listarProductosEstado(Estado estado) throws Exception;
+
+    @GetMapping("/lista")
+    List<ProductoGetDTO> listarProductoNombre(String nombre) throws Exception;
+
+    @GetMapping("/lista")
+    List<ProductoGetDTO> listarProductosPrecio(float precioMin, float precioMax) throws Exception;
+
+    @GetMapping("/lista")
+    List<ProductoGetDTO> listarProductoFavoritos(int codigoUsuario) throws Exception;
+
+    @GetMapping("/lista")
+    List<ProductoGetDTO> lsitarProductoPropio(int codigoUsuario)throws Exception;//listar los productos que el usuario publica
+
 
 }
